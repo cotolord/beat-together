7e3335527e6428511d613b9076bccf7de7b67a2f
cotolord <sdvste08@gmail.com>
2024-09-10 18:29:50 +0200

    Add files via upload

---
+++ beat-js.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const startButton = document.getElementById('start-button');
+
+    startButton.addEventListener('click', () => {
+        alert('Hai cliccato su Start!');
+    });
+});
